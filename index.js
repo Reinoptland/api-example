214260c6b91e6218b4c332d5126c93dc0192e12f
Reinoptland <reinoptland@gmail.com>
2022-08-11 13:10:29 +0200

    Add challenge

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+// METHOD: GET, PATH "/""
+app.get("/", (req, res) => {
+  res.send("HELLO WORLD");
+});
+
+// METHOD: GET, PATH "/products"
+// Send back: "PRODUCTS"
+
+// METHOD: POST, PATH "/products"
+// Send back: "NEW PRODUCT"
+
+// METHOD: PATCH, PATH "/products"
+// Send back: "UPDATE PRODUCT"
+
+// METHOD: DELETE, PATH "/products"
+// Send back: "DELETE SOMETHING"
+
+// How to test? use postman
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
